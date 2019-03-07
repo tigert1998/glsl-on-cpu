76c6953a776279824a8cf1d64ca93401ced9595f
tigertang <491506913@qq.com>
2019-03-07 16:25:04 +0800

    Write test code to output names of functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+class TestListener extends LangBaseListener {
+    public void enterFunctionDefinition(LangParser.FunctionDefinitionContext ctx) {
+        System.out.println(ctx.functionSignature().children.get(1).toString());
+    }
+}
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        String filePath = args[0];
+        LangLexer langLexer = new LangLexer(CharStreams.fromFileName(filePath));
+        LangParser langParser = new LangParser(new CommonTokenStream(langLexer));
+        ParseTree parseTree = langParser.prog();
+        ParseTreeWalker.DEFAULT.walk(new TestListener(), parseTree);
+    }
 }

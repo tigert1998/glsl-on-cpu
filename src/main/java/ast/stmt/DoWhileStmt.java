4602190efe701bd4e479c1731a6ccccc6ac1defc
tigertang <491506913@qq.com>
2019-05-19 10:08:18 +0800

    Add break continue for do while and while do

--- src/main/java/ast/stmt/DoWhileStmt.java
+++ src/main/java/ast/stmt/DoWhileStmt.java
@@ -1,9 +1,13 @@
 package ast.stmt;
 
+import ast.Scope;
 import ast.expr.Expr;
+import org.bytedeco.llvm.LLVM.*;
 import org.json.JSONObject;
 
-public class DoWhileStmt extends Stmt {
+import static org.bytedeco.llvm.global.LLVM.*;
+
+public class DoWhileStmt extends Stmt implements LoopStmt {
     public CompoundStmt body;
     public Expr condition;
     private ControlFlowManager controlFlowManager;
@@ -12,6 +16,38 @@ public class DoWhileStmt extends Stmt {
         this.body = body;
         this.condition = condition;
         this.controlFlowManager = controlFlowManager;
+    }
+
+    @Override
+    public LLVMValueRef evaluate(LLVMModuleRef module, LLVMValueRef function, Scope scope) {
+        var lastBlock = LLVMGetLastBasicBlock(function);
+
+        var bodyBlock = LLVMAppendBasicBlock(function, "body");
+        body.evaluate(module, function, scope);
+
+        var condBlock = LLVMAppendBasicBlock(function, "cond");
+        var value = condition.evaluate(module, function, scope);
+
+        var endBlock = LLVMAppendBasicBlock(function, "end");
+
+        var builder = LLVMCreateBuilder();
+        LLVMPositionBuilderAtEnd(builder, lastBlock);
+        LLVMBuildBr(builder, bodyBlock);
+
+        // body
+        LLVMPositionBuilderAtEnd(builder, LLVMGetPreviousBasicBlock(condBlock));
+        LLVMBuildBr(builder, condBlock);
+
+        // cond
+        LLVMPositionBuilderAtEnd(builder, LLVMGetPreviousBasicBlock(endBlock));
+        value = LLVMBuildLoad(builder, value, "");
+        value = LLVMBuildIntCast2(builder, value, LLVMInt1Type(), 0, "");
+        LLVMBuildCondBr(builder, value, bodyBlock, endBlock);
+
+        LLVMDisposeBuilder(builder);
+
+        LoopStmt.super.addContinueBreaks(controlFlowManager, condBlock, endBlock);
+        return null;
     }
 
     @Override
--- src/main/java/ast/stmt/WhileStmt.java
+++ src/main/java/ast/stmt/WhileStmt.java
@@ -1,9 +1,13 @@
 package ast.stmt;
 
+import ast.Scope;
 import ast.expr.Expr;
+import org.bytedeco.llvm.LLVM.*;
 import org.json.JSONObject;
 
-public class WhileStmt extends Stmt {
+import static org.bytedeco.llvm.global.LLVM.*;
+
+public class WhileStmt extends Stmt implements LoopStmt {
     public Expr condition;
     public CompoundStmt body;
     private ControlFlowManager controlFlowManager;
@@ -12,6 +16,38 @@ public class WhileStmt extends Stmt {
         this.condition = condition;
         this.body = body;
         this.controlFlowManager = controlFlowManager;
+    }
+
+    @Override
+    public LLVMValueRef evaluate(LLVMModuleRef module, LLVMValueRef function, Scope scope) {
+        var lastBlock = LLVMGetLastBasicBlock(function);
+
+        var condBlock = LLVMAppendBasicBlock(function, "cond");
+        var value = condition.evaluate(module, function, scope);
+
+        var bodyBlock = LLVMAppendBasicBlock(function, "body");
+        body.evaluate(module, function, scope);
+
+        var endBlock = LLVMAppendBasicBlock(function, "end");
+
+        var builder = LLVMCreateBuilder();
+        LLVMPositionBuilderAtEnd(builder, lastBlock);
+        LLVMBuildBr(builder, condBlock);
+
+        // cond
+        LLVMPositionBuilderAtEnd(builder, LLVMGetPreviousBasicBlock(bodyBlock));
+        value = LLVMBuildLoad(builder, value, "");
+        value = LLVMBuildIntCast2(builder, value, LLVMInt1Type(), 0, "");
+        LLVMBuildCondBr(builder, value, bodyBlock, endBlock);
+
+        // body
+        LLVMPositionBuilderAtEnd(builder, LLVMGetPreviousBasicBlock(endBlock));
+        LLVMBuildBr(builder, condBlock);
+
+        LLVMDisposeBuilder(builder);
+
+        LoopStmt.super.addContinueBreaks(controlFlowManager, condBlock, endBlock);
+        return null;
     }
 
     @Override

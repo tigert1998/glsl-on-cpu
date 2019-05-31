697b70cfd23fa5c5affc752f589ebcbd48017ac3
tigertang <tigertang@zju.edu.cn>
2019-05-31 19:53:41 +0800

    Add pikachu demo

--- demos/8_queens.c
+++ demos/8_queens.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-extern void solve(int *, char *);
+extern void GLSL_solve(int *, char *);
 extern int ans[8];
 
 int main() {
     int depth = 0;
     char res;
-    solve(&depth, &res);
+    GLSL_solve(&depth, &res);
     puts(res ? "yes" : "no");
     for (int i = 0; i < 8; i++) printf("%d, ", ans[i]);
     puts("");
---
+++ demos/pikachu.cc
@@ -0,0 +1,66 @@
+#include <cmath>
+#include <cstdio>
+
+extern "C"
+{
+    void GLSL_cos(float *x, float *ret);
+    void GLSL_sin(float *x, float *ret);
+    void GLSL_floor(float *x, float *ret);
+    void GLSL_pow(float *x, float *y, float *ret);
+    void GLSL_mainImage(float *(*fragColor)[4], float *(*fragCoord)[2]);
+    void GLSL_print(float *(*x)[4]);
+}
+
+extern float iResolution[3];
+
+void GLSL_print(float *(*x)[4])
+{
+    printf("(");
+    for (int i = 0; i < 4; i++)
+        printf("%.3f, ", *((*x)[i]));
+    printf(")\n");
+}
+
+void GLSL_cos(float *x, float *ret)
+{
+    *ret = std::cos(*x);
+}
+
+void GLSL_sin(float *x, float *ret)
+{
+    *ret = std::sin(*x);
+}
+
+void GLSL_floor(float *x, float *ret)
+{
+    *ret = std::floor(*x);
+}
+
+void GLSL_pow(float *x, float *y, float *ret)
+{
+    *ret = std::pow(*x, *y);
+}
+
+int main()
+{
+    float *fragCoord[2];
+    float *fragColor[4];
+    for (int i = 0; i < 4; i++)
+        fragColor[i] = new float();
+
+    printf("P3\n%d %d\n255\n", int(iResolution[0]), int(iResolution[1]));
+    for (float i = iResolution[1] - 1; i >= 0; i--)
+    {
+        fprintf(stderr, "\r%.3f%%...", 100 * (iResolution[1] - i) / iResolution[1]);
+        for (float j = 0; j < iResolution[0]; j++)
+        {
+            fragCoord[0] = &j;
+            fragCoord[1] = &i;
+            GLSL_mainImage(&fragColor, &fragCoord);
+            for (int k = 0; k < 3; k++)
+            {
+                printf("%d ", int(*fragColor[k] * *fragColor[3] * 255));
+            }
+        }
+    }
+}
